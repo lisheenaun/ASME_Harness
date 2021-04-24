ec5cc82a672196a747916661fb90fba229e4d5bb
Daniel-Slattery <daniel.slattery1@gmail.com>
2021-04-24 16:34:11 +0400

    changed file layout for flask and run.py

--- run.py
+++ run.py
@@ -5,13 +5,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
-    app.run(
-        host=os.environ.get("IP", "0.0.0.0"),
-        port=int(os.environ.get("PORT", "5000")),
-        debug=True)
+    app.run()
